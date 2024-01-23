339c343cfed0d237f48554467103ec31addb5b6f
cycl0n3-ga <jdio1881@gmail.com>
2024-01-23 01:06:17 -0800

    optimize publisher, add 台科大 where data is from 佈告欄

--- scraper.py
+++ scraper.py
@@ -15,6 +15,9 @@ db_config = {
     "port": "65432"
 }
 
+NTUST_LANG_URL = "https://lc.ntust.edu.tw/p/403-1070-1053-1.php?Lang=zh-tw"
+NTUST_INSIDE_URL = "https://bulletin.ntust.edu.tw/p/403-1045-1391-1.php?Lang=zh-tw"
+NTUST_OUTSIDE_URL = "https://www.ntust.edu.tw/p/403-1000-168-1.php?Lang=zh-tw"
 
 #靜態類別 輸入網址轉類別
 class ScraperFactory:
@@ -55,7 +58,7 @@ class NTUSTBulletinScraper(Scraper):
         
         for row in tbody.find_all("tr"):
             data_row = []       
-            publisher = row.find("td",{"data-th":"發佈單位"}).get_text(strip=True)
+            publisher = "台科大" + row.find("td",{"data-th":"發佈單位"}).get_text(strip=True)
             title = row.find("td",{"data-th":"標題"}).get_text(strip=True)
             a_tag = row.find("a")
             url = a_tag['href'] if a_tag and 'href' in a_tag.attrs else None
@@ -68,10 +71,6 @@ class NTUSTBulletinScraper(Scraper):
                     content += p.get_text(strip=True)
             yield {'publisher':publisher,'title':title,'url':url,'content':content}
 
-            
-        
-
-        
 
 
 # 台科大語言中心爬蟲
@@ -119,13 +118,9 @@ class NTUSTMajorAnnouncementScraper(Scraper):
             yield {'publisher':publisher,'title':title,'url':url,'content':content}    
 
 
-
-
-def scrape():
+def SaveToDB(data):
     connection = psycopg2.connect(**db_config)
     cursor = connection.cursor()
-    Scrape = ScraperFactory.get_scraper('https://bulletin.ntust.edu.tw/p/403-1045-1391-1.php?Lang=zh-tw')
-    data = Scrape.scrape()
     for row in data:
         publisher = row.get('publisher')
         title = row.get('title')
@@ -160,13 +155,26 @@ def scrape():
 
                 print("Data inserted successfully!")
 
-
         except (Exception, psycopg2.Error) as error:
             print("Error while connecting to PostgreSQL:", error)
 
-
-
     connection.close()
+
+def scrape():
+    
+    Scrape = ScraperFactory.get_scraper(NTUST_INSIDE_URL)
+    data = Scrape.scrape()
+    SaveToDB(data)
+
+    Scrape = ScraperFactory.get_scraper(NTUST_LANG_URL)
+    data = Scrape.scrape()
+    SaveToDB(data)
+
+    Scrape = ScraperFactory.get_scraper(NTUST_OUTSIDE_URL)
+    data = Scrape.scrape()
+    SaveToDB(data)
+
+   
 
 
 if __name__ == '__main__':
