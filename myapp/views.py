e7d19dbbfc56bb9a47e83b16d8c4f1d4e9c4362c
Leo <lion24161582@gmail.com>
2024-01-23 15:28:49 +0800

    三個校網的爬蟲邏輯完成

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,5 @@ def index(request):
     if request.method == 'POST':
         url = request.POST.get('website')
         scraper = ScraperFactory.get_scraper(url)
-        # data.append({'date':date,'publisher':publisher,'title':title,'url':url,'content':content})
         data = scraper.scrape() 
     return render(request, 'index.html', {'data': data})# render(request,'index.html',{'data':data})
