32e6855a5ec403f68c2ab1867cbecc9a7d576fc2
cai-yihua <jess92725@gmail.com>
2024-03-04 23:35:03 +0800

    原始公告取得、修改、刪除

--- api/api.py
+++ api/api.py
@@ -20,7 +20,6 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 load_dotenv()
 
-
 # make sure you have run the following command before testing!
 # ssh -L 65432:localhost:65432 mitlab@140.118.2.52 -p 33700
 # uvicorn api:app --reload
@@ -46,6 +45,7 @@ app.add_middleware(
     allow_methods=["*"],  # 允許所有 HTTP 方法
     allow_headers=["*"],  # 允許所有 HTTP 標頭
 )
+
 @app.get("/")
 async def root():
     return RedirectResponse(url='/docs')
@@ -93,7 +93,8 @@ class NewDataReturn(BaseModel):
 # ORDER BY Score DESC, addtime DESC, id ASC;
 # 輸入台網
 
-def adjust_date_range(start_date_str,end_date_str):
+# 日期範圍調整
+def adjust_date_range(start_date_str, end_date_str):
     logger.info(f"input {start_date_str}~{end_date_str}")
     # 將字符串轉換為 datetime 對象
     start_date_o = datetime.strptime(start_date_str, "%Y-%m-%d")
@@ -103,51 +104,53 @@ def adjust_date_range(start_date_str,end_date_str):
     # 如果需要，將 datetime 對象轉換回字符串
     end_date_str = end_date_o.strftime("%Y-%m-%d")
     logger.info(f"return {start_date_str}~{end_date_str}")
-    return start_date_str,end_date_str
-def create_sql_query(keywords,number_data,start_date,end_date):
+    return start_date_str, end_date_str
+
+def create_sql_query(keywords, number_data, start_date, end_date):
     case_statements = []
     where_conditions = []
     params = []
-    if start_date is not None and end_date is not None :#調整時間！
-        start_date,end_date = adjust_date_range(start_date,end_date)
-    if keywords:#有關鍵字
-        for  keyword in keywords:
+    if start_date is not None and end_date is not None: # 調整時間
+        start_date, end_date = adjust_date_range(start_date, end_date)
+    if keywords: # 有關鍵字
+        for keyword in keywords:
             param = f"%{keyword}%"
             case_statements.append(f"(CASE WHEN publisher LIKE %s THEN 1 ELSE 0 END)")
             where_conditions.append(f"publisher LIKE %s")
             params.append(param)
-        params.extend(params)# 參數再重複一次,原本是[台,網],重複一次就變成 [台,網,台,網] 為了後面SQL搜索時運用到避免遇到SQL注入攻擊
-        params.extend([number_data] if start_date is None or end_date is None  else  [start_date,end_date,number_data])
+        params.extend(params) # 參數再重複一次,原本是[台,網],重複一次就變成 [台,網,台,網] 為了後面SQL搜索時運用到避免遇到SQL注入攻擊
+        params.extend([number_data] if start_date is None or end_date is None  else [start_date, end_date, number_data])
         case_sql = " + ".join(case_statements)
         where_sql = " OR ".join(where_conditions)
         where_date_sql = "" if start_date is None and end_date is None else f"AND ( addtime >= %s AND addtime < %s)"
 
         sql_query = f"""
-        SELECT *,
+            SELECT *,
             ({case_sql}) AS Score
-        FROM public.bulletinraw
-        WHERE ({where_sql}) {where_date_sql}
-        ORDER BY Score DESC, addtime DESC
-        LIMIT %s;
+            FROM public.bulletinraw
+            WHERE ({where_sql}) {where_date_sql}
+            ORDER BY Score DESC, addtime DESC
+            LIMIT %s;
         """ 
-    else :#沒有關鍵字
+    else: # 沒有關鍵字
         where_date_sql = "" if start_date is None or end_date is None else f"WHERE ( addtime >= %s AND addtime < %s)"
         sql_query = f"""
-                SELECT * FROM bulletinraw
-                {where_date_sql}
-                ORDER BY addtime DESC
-                LIMIT %s;
-            """
-        params.extend([number_data] if start_date is None or end_date is None  else  [start_date,end_date,number_data] )
-    logger.debug(f"sql_query:{sql_query},params:{params}")
+            SELECT * FROM bulletinraw
+            {where_date_sql}
+            ORDER BY addtime DESC
+            LIMIT %s;
+        """
+        params.extend([number_data] if start_date is None or end_date is None  else  [start_date, end_date, number_data] )
+    logger.debug(f"sql_query:{sql_query}, params:{params}")
     return sql_query, params
+
 # 取得最新的幾筆資料
-## 取得的格式如下 ((ID,發布者,url,content,上傳時間),()...,)
-def fetch_data(category: str,numbers:int, start_date: str, end_date: str):
-    logger.debug(f"category:{category},numbers:{numbers},start_date:{start_date},end_date:{end_date}")
+# 取得的格式如下 ((rawid, publisher, title, url, content, addtime),()...,)
+def fetch_data(keywords: str, numbers: int, start_date: str, end_date: str):
+    logger.debug(f"keywords:{keywords}, numbers:{numbers}, start_date:{start_date}, end_date:{end_date}")
     connection = psycopg2.connect(**db_config)
     cursor = connection.cursor()
-    SQL_query ,params= create_sql_query(category,numbers,start_date,end_date)
+    SQL_query ,params = create_sql_query(keywords, numbers, start_date, end_date)
     query = sql.SQL(
         SQL_query
     )
@@ -158,20 +161,22 @@ def fetch_data(category: str,numbers:int, start_date: str, end_date: str):
         connection.close()
         print("PostgreSQL connection is closed")
     return records
+
 # 測試過中文的參數進入fastapi會自動處理不須轉換
-@app.get("/api/getdata", response_model=List[Post])
-async def get_data(category: Optional[str] = None, start_date:Optional[str]= None,end_date:Optional[str]= None,numbers: int = Query(default=20, le=200)):
+# 取得原始公告資料  
+@app.get("/frontend/get_bulletin", response_model=List[Post])
+async def get_bulletin(keywords: Optional[str]=None, start_date: Optional[str]=None, end_date:Optional[str]=None, numbers: int=Query(default=20, le=200)):
     try:
-        logger.info(f"Fetching {numbers} latest posts from {category} ,Date:{start_date}~{end_date}")
-        raw_data = fetch_data(category, numbers,start_date,end_date)  # ((id, publisher,),....)
+        logger.info(f"Fetching {numbers} latest posts from {keywords}, Date:{start_date}~{end_date}")
+        raw_data = fetch_data(keywords, numbers, start_date, end_date)  # ((id, publisher,),....)
         data = [
             Post(
-                rawid=row[0],
-                publisher=row[1],
-                title=row[2],
-                url=row[3],
-                content=row[4],
-                addtime=row[5],
+                rawid = row[0],
+                publisher = row[1],
+                title = row[2],
+                url = row[3],
+                content = row[4],
+                addtime = row[5],
             )
             for row in raw_data
         ]
@@ -181,9 +186,89 @@ async def get_data(category: Optional[str] = None, start_date:Optional[str]= Non
         error_traceback = traceback.format_exc()
         logger.error("%s\n%s", error_message, error_traceback)
         return JSONResponse(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": str(Error),"detail":error_traceback},
+        )
+
+# 刪除原始公告資料
+@app.post("/frontend/delete_bulletin")
+async def delete_bulletin(rawid: int):
+    logger.info(f"Deleting data {rawid} from the database")
+    try:
+        connection = psycopg2.connect(**db_config)
+        cursor = connection.cursor()
+
+        # 查詢公告是否存在
+        cursor.execute("""
+            DELETE FROM bulletinraw
+            WHERE rawid = %s
+            RETURNING rawid
+        """, (rawid,))
+        # 獲取刪除的行數
+        deleted_rows = cursor.rowcount
+        # 檢查是否刪除成功
+        if deleted_rows > 0:
+            connection.commit()
+            logger.info(f"Deletion completed")
+            return {"message": "Data successfully deleted"}
+        else:
+            # 如果公告不存在，則引發HTTP異常
+            raise HTTPException(status_code=404, detail="Data not found")
+
+    except Exception as Error:
+        error_message = "An error occurred during deletion: {}".format(str(Error))
+        error_traceback = traceback.format_exc()
+        logger.error("%s\n%s", error_message, error_traceback)
+        return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": str(Error),"detail":error_traceback},
         )
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+# 修改原始公告資料
+@app.post("/frontend/modify_bulletin/{rawid}")
+async def modify_bulletin(rawid: int, post: PostIn):
+    logger.info(f"Modifying bulletin with rawid: {rawid}")
+    try:
+        connection = psycopg2.connect(**db_config)
+        cursor = connection.cursor()
+
+        # 檢查要修改的公告是否存在
+        cursor.execute("""
+            SELECT * FROM bulletinraw
+            WHERE rawid = %s
+        """, (rawid,))
+        existing_post = cursor.fetchone()
+
+        if existing_post:
+            # 修改公告資料
+            cursor.execute("""
+                UPDATE bulletinraw
+                SET publisher = %s, title = %s, url = %s, content = %s, addtime = %s
+                WHERE rawid = %s
+            """, (post.publisher, post.title, post.url, post.content, post.addtime, rawid))
+            connection.commit()
+            logger.info(f"Modification in progress")
+            return {"message": "Data is being modified"}
+        else:
+            # 如果公告不存在，則引發 HTTP 異常
+            raise HTTPException(status_code=404, detail="Data not found")
+
+    except Exception as Error:
+        error_message = "Error occurring during modification: {}".format(str(Error))
+        error_traceback = traceback.format_exc()
+        logger.error("%s\n%s", error_message, error_traceback)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": str(Error),"detail":error_traceback},
+        )
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
 
 @app.post("/scraper/save_bulletin")
 async def save_data(post: PostIn):
@@ -221,8 +306,6 @@ async def save_data(post: PostIn):
         if connection:
             cursor.close()
             connection.close()
-
-
 
 @app.post("/bot/register_user")
 async def register_user(post: NewUser):
@@ -375,6 +458,7 @@ async def list_subscription(post:UserId):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": str(Error),"detail":error_traceback},
         )
+
 @app.post("/bot/get_user")#, response_model=List[ListSubTable])
 async def get_user():
     try:
@@ -397,7 +481,6 @@ async def get_user():
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": str(Error),"detail":error_traceback},
         )
-
 
 @app.post("/bot/get_newdata", response_model=List[NewDataReturn])
 async def get_newdata():
@@ -454,7 +537,6 @@ async def start_scraper():
         logger.info(f"Start Scraper")
         os.system("python3 ../scraper.py")  # call scraper
 
-
         return "Done"
     except Exception as Error:
         error_message = "Error occurred: {}".format(str(Error))
@@ -464,6 +546,7 @@ async def start_scraper():
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": str(Error),"detail":error_traceback},
         )
+
 
 
 if __name__ == "__main__":
