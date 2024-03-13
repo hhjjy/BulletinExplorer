6eaf7ba0b18ec2ebf0d9bd54a66aeb726ef8fbac
Leo <lion24161582@gmail.com>
2024-03-13 13:56:53 +0000

    requirement 更新 、改成llm

--- llm/llm.py
+++ llm/llm.py
@@ -1,16 +1,17 @@
-from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
 import os,asyncio,json,re,requests
 from dotenv import load_dotenv
 from log_config import setup_logger
 import logging,traceback
+from openai import AsyncOpenAI
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
     raise ValueError("请设置 OPENAI_API_KEY 环境变量。")
 logger = setup_logger("llm_service", log_level=logging.DEBUG)
-
+client = AsyncOpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 # label table的所有資料
 label_table = None
 def get_label_table():
@@ -20,50 +21,8 @@ def get_label_table():
 
 class LLMService:
     def __init__(self,api_endpoint):
-        self.llm = ChatOpenAI(temperature=0.3, model_name="gpt-4-0613")
         self.api_endpoint = api_endpoint
-
-    async def classify_content_by_llm(self, title: str, content: str) -> dict:
-        """異步分類內容並返回標籤字典"""
-        logger.info(f"輸入標題: {title}, 內容: {content}")
-        # 創建提示模板（此處假設這一步不需要異步操作）
-        templates_classify = """
-        ### 指令內容：
-        你是一個"對公告欄標題、內文分類成對應的標籤的工作者"。請以"繁體中文"輸出並按照下面的步驟執行：
-        1. 請依照要求將文章分成不同段落，一步一步將每個段落依序列出該段的主旨，再從中提取摘要，並根據"標籤定義"，列出該段符合的所有標籤。具體操作如下：
-            - **段落分割：** 閱讀文章內容，將其分割成不同的段落。每個段落應該集中於一個特定的主題或活動。
-            - **主旨分析：** 對每個獨立的段落進行主旨分析，明確該段落主要討論或描述的內容是什麼。
-            - **摘要提取：** 從每個段落中提取關鍵信息作為摘要，信息包括活動的時間、地點、參與條件、以及可能的獎勵等。
-            - **標籤匹配：** 根據事先定義的標籤類別，將每個段落的主旨和摘要與相應的標籤進行匹配，確定該段落屬於哪一個或哪幾個標籤分類。
-
-        2. 從出現過的標籤，把合理的標籤內容合併到一起，並把最終結果以JSON格式輸出。如果最終結果沒有任何標籤，請輸出標籤"其他"，否則不輸出標籤"其他"。具體步驟如下：
-            - **標籤合併：** 根據上一步驟中每個段落所匹配的標籤，將合理的標籤進行合併，形成一個包含所有相關標籤的集合。
-            - **JSON格式輸出：** 將合併後的標籤集合轉換為JSON格式，這些標籤將包含在tags鍵值下。如果經過分析後，某個公告內容不適合任何預定義的標籤，則為該公告分配一個"其他"標籤。
-
-        ### 標籤與標籤定義：
-        "餐點": "提供免費食物或餐點讓大家吃",
-        "自我認識": "提供心理健康相關服務，如個別諮詢、小團體訓練、心理測驗、生命教育和性平教育，但不包含透過分享引起他人的反饋和討論",
-        "考試": "用於評估學生學習成果的正式測試，包括書面、口頭或實踐測試，通過後可獲得證照或證書",
-        "學習技能": "提升個人能力、知識或專業技能",
-        "體育活動": "指可參加的體育相關競賽",
-        "學校舉辦活動": "學校為促進學習、社群感及慶祝成就而策劃的事件，包括校慶、電影、文化節等。這些活動旨在支持學生全面發展，如身心健康和社交技巧，並提供展示才能的平台",
-        "英文學分": "通過完成英語課程或程序獲得的學分",
-        "付費或繳款": "需要在繳費日期前繳交費用，並且繳費日期在活動日期前，包括學雜費、活動保證金、申請車位等，不包含活動中獲得的獎勵"
-        ### 用戶輸入:
-        標題:{title}
-        內文:{content}
-        """        
-        prompt = PromptTemplate.from_template(templates_classify)
-        formatted_prompt = prompt.format(title=title, content=content)
-        logger.info(f"格式化提示: {formatted_prompt}")
-        
-        # 假設 ChatOpenAI 的 invoke 方法支持異步調用
-        response = await self.llm.invoke(formatted_prompt)
-        logger.info(f"輸出: {response}")
-        
-        tags_dict = self.extract_json_from_response(response)
-        return tags_dict
-
+        # openai.api_key = os.getenv("OPENAI_API_KEY")
     async def classify_content_by_regex(self, title: str, content: str) -> dict:
         result = {'tags': []}
         global label_table
@@ -125,22 +84,86 @@ class LLMService:
         result['tags'] = regex_labels.get('tags', []) + voting_result.get('tags', [])
         return result
 
-    async def aggregate_tags_from_models(self, rawid, title, content):
-        # 假设 BulletinInfo 包含了 rawid, title, content
-        bulletin_info = {"rawid": rawid, "title": title, "content": content}
+    async def llm(self,title: str, content: str):
+        client = AsyncOpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+        )
+        # 使用提供的模板格式化输入
+        system_prompt  = f"""
+            ### 指令內容：
+            你是一個"對公告欄標題、內文分類成對應的標籤的工作者"。請以"繁體中文"輸出並按照下面的步驟執行：
+            1. 請依照要求將文章分成不同段落，一步一步將每個段落依序列出該段的主旨，再從中提取摘要，並根據"標籤定義"，列出該段符合的所有標籤。具體操作如下：
+                - **段落分割：** 閱讀文章內容，將其分割成不同的段落。每個段落應該集中於一個特定的主題或活動。
+                - **主旨分析：** 對每個獨立的段落進行主旨分析，明確該段落主要討論或描述的內容是什麼。
+                - **摘要提取：** 從每個段落中提取關鍵信息作為摘要，信息包括活動的時間、地點、參與條件、以及可能的獎勵等。
+                - **標籤匹配：** 根據事先定義的標籤類別，將每個段落的主旨和摘要與相應的標籤進行匹配，確定該段落屬於哪一個或哪幾個標籤分類。
+
+            2. 從出現過的標籤，把合理的標籤內容合併到一起，並把最終結果以JSON格式輸出。如果最終結果沒有任何標籤，請輸出標籤"其他"，否則不輸出標籤"其他"。具體步驟如下：
+                - **標籤合併：** 根據上一步驟中每個段落所匹配的標籤，將合理的標籤進行合併，形成一個包含所有相關標籤的集合。
+                - **JSON格式輸出：** 將合併後的標籤集合轉換為JSON格式，這些標籤將包含在tags鍵值下。如果經過分析後，某個公告內容不適合任何預定義的標籤，則為該公告分配一個"其他"標籤。
+
+            ### 標籤與標籤定義：
+            "餐點": "提供免費食物或餐點讓大家吃",
+            "自我認識": "提供心理健康相關服務，如個別諮詢、小團體訓練、心理測驗、生命教育和性平教育，但不包含透過分享引起他人的反饋和討論",
+            "考試": "用於評估學生學習成果的正式測試，包括書面、口頭或實踐測試，通過後可獲得證照或證書",
+            "學習技能": "提升個人能力、知識或專業技能",
+            "體育活動": "指可參加的體育相關競賽",
+            "學校舉辦活動": "學校為促進學習、社群感及慶祝成就而策劃的事件，包括校慶、電影、文化節等。這些活動旨在支持學生全面發展，如身心健康和社交技巧，並提供展示才能的平台",
+            "英文學分": "通過完成英語課程或程序獲得的學分",
+            "付費或繳款": "需要在繳費日期前繳交費用，並且繳費日期在活動日期前，包括學雜費、活動保證金、申請車位等，不包含活動中獲得的獎勵"
+            """        
+        user_input = f"""###用戶輸入:
+            "標題": "{title}",
+            "內文": "{content}"
+            """
+        chat_completion = await client.chat.completions.create(
+            model="gpt-4-0613",
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt,
+                },
+                {
+                    "role": "user",
+                    "content": user_input,
+                },
+                # 根据需要添加更多消息
+            ],
+        )
+        # 从返回的对象中提取消息内容
+        if chat_completion.choices:
+            message_content = chat_completion.choices[0].message.content  # 注意这里的改动
+        else:
+            message_content = "No completion choices were returned."
+        return message_content
+    
+    # async def aggregate_tags_from_models(self, rawid, title, content):
+    #     # 假设 BulletinInfo 包含了 rawid, title, content
+    #     bulletin_info = {"rawid": rawid, "title": title, "content": content}
         
-        # 对 BulletinInfo 进行处理，这里 LLM1, LLM2, LLM3 是示例处理函数
-        # 你需要根据实际情况定义这些函数或者处理逻辑
-        results_llm1 = self.LLM1(bulletin_info)
-        results_llm2 = self.LLM2(bulletin_info)
-        results_llm3 = self.LLM3(bulletin_info)
+    #     # 对 BulletinInfo 进行处理，这里 LLM1, LLM2, LLM3 是示例处理函数
+    #     # 你需要根据实际情况定义这些函数或者处理逻辑
+    #     results_llm1 = self.llm(bulletin_info)
+    #     results_llm2 = self.llm(bulletin_info)
+    #     results_llm3 = self.llm(bulletin_info)
         
-        # 将结果提交给投票系统，这里的 voting 是一个示例函数
-        # 你需要根据实际情况定义投票或聚合逻辑
-        final_tags = self.voting([results_llm1, results_llm2, results_llm3])
+    #     # 将结果提交给投票系统，这里的 voting 是一个示例函数
+    #     # 你需要根据实际情况定义投票或聚合逻辑
+    #     final_tags = self.voting([results_llm1, results_llm2, results_llm3])
         
-        # 返回最终的标签
-        return {"tags": final_tags}        
+    #     # 返回最终的标签
+    #     return {"tags": final_tags}   
+    async def test(self,title,content):
+        llm1_labels = await self.llm(title, content)
+        llm2_labels = await self.llm(title, content)
+        llm3_labels = await self.llm(title, content)
+        print(llm1_labels)
+        print(llm2_labels)
+        print(llm3_labels)
+        logger.info(f"llm1,2,3:{llm1_labels},{llm2_labels},{llm3_labels}")
+        llm_voting_result = self.voting(llm1_labels, llm2_labels, llm3_labels)
+        logger.info(f"llm_voting_result:{llm_voting_result}")
+        
     async def execute(self):
         # Fetch unprocessed bulletins
         bulletins_response = requests.post(f"{self.api_endpoint}/llm/get_unprocessed_bulletin")
@@ -149,42 +172,41 @@ class LLMService:
             rawid = bulletin['rawid']
             title = bulletin['title']
             content = bulletin['content']
-
             # Classify content using three different LLMs and perform voting
-            llm1_labels = await self.classify_content_by_llm(title, content)
-            llm2_labels = await self.classify_content_by_llm(title, content)
-            llm3_labels = await self.classify_content_by_llm(title, content)
+            llm1_labels = await self.llm(title, content)
+            llm2_labels = await self.llm(title, content)
+            llm3_labels = await self.llm(title, content)
+            logger.info(f"llm1,2,3:{llm1_labels},{llm2_labels},{llm3_labels}")
             llm_voting_result = self.voting(llm1_labels, llm2_labels, llm3_labels)
-
+            logger.info(f"llm_voting_result:{llm_voting_result}")
             # Classify content using regex
-            labels_regex = await self.classify_content_by_regex(title, content)
+            # labels_regex = await self.classify_content_by_regex(title, content)
 
-            # Combine results
-            result_label = await self.combine(labels_regex, llm_voting_result)
+            # # Combine results
+            # result_label = await self.combine(labels_regex, llm_voting_result)
 
-            # Post labels back to the API
-            for tag in result_label['tags']:
+            # # Post labels back to the API
+            # for tag in result_label['tags']:
                 
-                label_response = await requests.post(f"{self.api_endpoint}/llm/get_label_id", json={'labelname': tag})
-                label_id = label_response.json()['labelid']
-                response_dict = {'rawid': rawid, 'labelid': label_id}
-                await requests.post(f"{self.api_endpoint}/llm/save_label", json=response_dict)
+            #     label_response = await requests.post(f"{self.api_endpoint}/llm/get_label_id", json={'labelname': tag})
+            #     label_id = label_response.json()['labelid']
+            #     response_dict = {'rawid': rawid, 'labelid': label_id}
+            #     await requests.post(f"{self.api_endpoint}/llm/save_label", json=response_dict)
 
         # Report finishing
         requests.post(f"{self.api_endpoint}/llm/report_finishing")
 
     @staticmethod
     def extract_json_from_response(response: str) -> dict:
-        """從字符串中提取 JSON 並返回字典"""
-        logger.info(f"輸入: {response}")
-        pattern = r'```json\n(.*?\n*?)```'
+        logger.info(f"input : {response}")
+        """从字符串中提取 JSON 并返回字典"""
+        # 匹配被 ```json\n 和 ``` 包裹的，或直接以 { 开头直到 } 结尾（考虑嵌套的情况）的 JSON 字符串
+        pattern = r'```json\n([\s\S]*?)```|(\{[\s\S]*?\}(?![\s\S]*\}))'
         matches = re.search(pattern, response, re.DOTALL)
-        
-        json_content_str = matches.group(1) if matches else '{"tags": ["其他"]}'
-        logger.info(f"提取的 JSON 字串: {json_content_str}")
-        
+        # 如果匹配到被 ``` 包裹的 JSON，使用第一个匹配组；否则，使用第二个匹配组
+        json_content_str = matches.group(1) if matches.group(1) else (matches.group(2) if matches.group(2) else '{"tags": ["其他"]}')
         response_dict = json.loads(json_content_str)
-        logger.info(f"輸出: {response_dict}")
+        logger.info(f"output : {response_dict}")
         return response_dict
 
 if __name__ == '__main__':
@@ -193,7 +215,9 @@ if __name__ == '__main__':
     # content='### 指令操作：\n\n#### 段落分割：\n- 段落1：【學務處生輔組】112-2新增【校外】、【自辦】獎助學金，詳見獎學金網頁/最新消息。\n- 段落2：© Copyright (c) NTUST 2020© 學務處生活輔導組電話：(02)2730-3760© 系統開發及維護：電子計算機中心\n\n#### 主旨分析：\n- 段落1主旨：學務處生輔組在112-2學期新增了校外和自辦的獎助學金，詳細信息可以在獎學金網頁的最新消息中查看。\n- 段落2主旨：版權所有，學務處生活輔導組的聯絡電話和系統開發及維護單位的信息。\n\n#### 摘要提取：\n- 段落1摘要：112-2學期新增校外和自辦的獎助學金，詳情請查看獎學金網頁的最新消息。\n- 段落2摘要：學務處生活輔導組的聯絡電話為(02)2730-3760，系統由電子計算機中心開發及維護。\n\n#### 標籤匹配：\n- 段落1標籤：學校舉辦活動\n- 段落2標籤：其他\n\n### 指令操作：\n\n#### 標籤合併：\n- 學校舉辦活動、其他\n\n#### JSON格式輸出：\n```json\n{\n    "tags": ["學校舉辦活動", "其他"]\n}\n```'
     # print(get_dict_from_str(content))
     LLM = LLMService("http://127.0.0.1:8000")
-    
+    title = "【學務處生輔組】112-2新增【校外】、【自辦】獎助學金，詳見獎學金網頁/最新消息。"
+    content = "© Copyright (c) NTUST 2020© 學務處生活輔導組電話：(02)2730-3760© 系統開發及維護：電子計算機中心"
+    asyncio.run(LLM.test(title,content))
     # import os
     # import asyncio
     # from openai import AsyncOpenAI
@@ -228,4 +252,4 @@ if __name__ == '__main__':
         # )
 
 
-    asyncio.run(llm(1,2))
+    
